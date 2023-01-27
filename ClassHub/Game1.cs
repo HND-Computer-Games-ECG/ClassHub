3161f32e9acbe0163665a2a32fafc78b85b6010d
Nigel Kennington <nigel.kennington@gmail.com>
2023-01-27 11:41:48 +0000

    Placeholders

--- ClassHub/Game1.cs
+++ ClassHub/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace ClassHub
 {
@@ -8,6 +9,11 @@ namespace ClassHub
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private List<string> names;
+        private SpriteFont placeholderText;
+
+        private MouseState ms_curr;
 
         public Game1()
         {
@@ -18,7 +24,28 @@ namespace ClassHub
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            names = new List<string>
+            {
+                "Nigel",
+                "Kristopher",
+                "Jonathan",
+                "Duncan",
+                "Harry",
+                "Declan",
+                "Kamil",
+                "Jakub",
+                "Shannon",
+                "Deuie",
+                "Noah",
+                "Ross",
+                "Alvin",
+                "Lewis",
+                "Gastao",
+                "MateuszS",
+                "Juan",
+                "MateuszW",
+                "Peter"
+            };
 
             base.Initialize();
         }
@@ -27,24 +54,93 @@ namespace ClassHub
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            placeholderText = Content.Load<SpriteFont>("Arial07");
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
+            ms_curr = Mouse.GetState();
 
             base.Update(gameTime);
+        }
+
+        private void drawPlaceholder(int x, int y, int size, string label)
+        {
+            HelperFuncs.DrawRect(GraphicsDevice, _spriteBatch, new Rectangle(x, y, size, size), Color.Black);
+            _spriteBatch.DrawString(placeholderText, label, new Vector2(x + size - 2, y + size - 2) - placeholderText.MeasureString(label), Color.Black);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            // Placeholders for the unused spaces
+            int x = 40, y = 24;
+            int size = 92, spacing = 24;
+            int rowLength = 6;
+            int i = 0;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+            drawPlaceholder(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, names[i]);
+            i++;
+
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ ClassHub/HelperFunctions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassHub
+{
+    public static class HelperFuncs
+    {
+        /// <summary>
+        /// Creates a texture that is a single white pixel
+        /// </summary>
+        /// <param name="gd">The current game's graphics device</param>
+        /// <returns></returns>
+        public static Texture2D GeneratePixel(GraphicsDevice gd)
+        {
+            Texture2D texture = new Texture2D(gd, 1, 1);
+            Color[] data = { Color.White };
+            texture.SetData(data);
+            return texture;
+        }
+
+        public static void DrawRect(GraphicsDevice gd, SpriteBatch sb, Rectangle rect, Color tint)
+        {
+            var pixel = GeneratePixel(gd);
+
+            // Pale body fill
+            sb.Draw(pixel, rect, tint * 0.1f);
+
+            // Borders (internal)
+            sb.Draw(pixel, new Rectangle(rect.Left, rect.Top, rect.Width, 1), tint * 0.8f);
+            sb.Draw(pixel, new Rectangle(rect.Left, rect.Top, 1, rect.Height), tint * 0.8f);
+            sb.Draw(pixel, new Rectangle(rect.Left, rect.Bottom-1, rect.Width, 1), tint * 0.8f);
+            sb.Draw(pixel, new Rectangle(rect.Right-1, rect.Top, 1, rect.Height), tint * 0.8f);
+        }
+    }
+}
