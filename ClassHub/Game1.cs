746ee78e871dd03cbd00ad37aa025ecd3c4a4dbd
ec2064817 <ec2064817@edinburghcollege.ac.uk>
2023-02-09 11:00:21 +0000

    Added Mackenzie

--- ClassHub/Game1.cs
+++ ClassHub/Game1.cs
@@ -82,7 +82,7 @@ namespace ClassHub
             placeHolders.Add(new Placeholder(new Rectangle(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, size), names[i]));
             i++;
 
-            placeHolders.Add(new Placeholder(new Rectangle(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, size), names[i]));
+            clickyButtons.Add(new ClickyButton(Content.Load<Texture2D>("Kenzie"), x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing)));
             i++;
 
             placeHolders.Add(new Placeholder(new Rectangle(x + (i % rowLength) * (size + spacing), y + (i / rowLength) * (size + spacing), size, size), names[i]));
